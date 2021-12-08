d31fa6c905a963157ab87b5ceabcc7d7a2485a3e
BandiRevanth <dhanush24082014@gmail.com>
2021-12-08 13:41:54 +0530

    Create Hello-World.asm

---
+++ Hello-World.asm
@@ -0,0 +1,13 @@
+(* Assembly *)
+
+
+global _main
+    extern _printf
+
+    section .text
+_main:
+    push    message
+    call    _printf
+    add        esp, 4
+message:
+    db    'Hello World', 10, 0
